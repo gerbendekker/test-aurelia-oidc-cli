8fe898800b74bf22bc196bf8ba5c292a5f4e6471
Gerben Dekker <gerbendekker@live.nl>
2018-03-12 14:52:25 +0100

    test cli requirejs success

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import {Aurelia} from 'aurelia-framework'
+import environment from './environment';
+import oidcConfig from "./open-id-connect-configuration";
+
+
+export function configure(aurelia: Aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .feature('resources')
+    .plugin("aurelia-open-id-connect", () => oidcConfig);
+
+  if (environment.debug) {
+    aurelia.use.developmentLogging();
+  }
+
+  if (environment.testing) {
+    aurelia.use.plugin('aurelia-testing');
+  }
+
+  aurelia.start().then(() => aurelia.setRoot());
+}
